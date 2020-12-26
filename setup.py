cbf54412db3921f10b9c68189ca2d32521f1b96c
Martin Brajer <martin.brajer@seznam.cz>
2020-12-26 17:18:27 +0100

    pypa setup

--- setup.py
+++ setup.py
@@ -4,19 +4,28 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE",  # Replace with your own username
+    name="physicslab",
     version="0.1.0",
     author="Martin Brajer",
     author_email="martin.brajer@seznam.cz",
-    description="A small example package",
+    description="Physics experiments evaluation.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/martin-brajer/sampleproject",
+    url="https://github.com/martin-brajer/physics-lab",
     packages=setuptools.find_packages(),
+    license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Physics",
     ],
+    keywords='physics',
     python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
 )
