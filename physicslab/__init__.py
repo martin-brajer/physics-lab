d5e100ca721934c5a8c6a48bac3d582a9cb55e57
Martin Brajer <martin.brajer@seznam.cz>
2020-12-26 17:42:00 +0100

    test release 0.1.1

--- physicslab/__init__.py
+++ physicslab/__init__.py
@@ -3,7 +3,7 @@ Physics Lab package helps with common experiments.
 """
 
 # __all__ []
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Martin Brajer'
 
 import physicslab.experiment
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="physicslab",
-    version="0.1.0",
+    version="0.1.1",
     author="Martin Brajer",
     author_email="martin.brajer@seznam.cz",
     description="Physics experiments evaluation.",
