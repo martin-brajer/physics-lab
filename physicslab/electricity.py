caf49ecffcebf5b2cc5a7d85ce7b6bb580bac2b5
Martin Brajer <martin.brajer@seznam.cz>
2020-12-29 12:47:58 +0100

    electricity properties structure

--- physicslab/electricity.py
+++ physicslab/electricity.py
@@ -4,14 +4,73 @@ Electricity
 # __all__ =
 
 
-def resistivity(sheet_resistance, thickness):
-    """Find resistivity from sheet resistance.
+class Resistance:
+    """ Object property.
 
-    :param sheet_resistance: (ohms per square)
-    :type sheet_resistance: float
-    :param thickness: (metres)
-    :type thickness: float
-    :return: Resistivity (ohms metres)
-    :rtype: float
+    SI unit: ohm
     """
-    return sheet_resistance * thickness
+
+    #: SI unit.
+    UNIT = 'ohm'
+
+    @staticmethod
+    def from_ohms_law(voltage, current):
+        """Find resistivity from sheet resistance.
+
+        :param voltage: (volt)
+        :type voltage: float
+        :param current: (ampere)
+        :type current: float
+        :return: (ohm)
+        :rtype: float
+        """
+        return voltage / current
+
+
+class Sheet_Resistance:
+    """ Thin object property.
+
+    """
+
+    #: SI unit.
+    UNIT = 'ohms per square'
+
+    pass
+
+
+class Resistivity:
+    """ Material property.
+
+    SI unit: ohm-meter
+    """
+
+    #: SI unit.
+    UNIT = 'ohm-meter'
+
+    @staticmethod
+    def from_sheet_resistance(sheet_resistance, thickness):
+        """Find resistivity from sheet resistance.
+
+        :param sheet_resistance: (ohms per square)
+        :type sheet_resistance: float
+        :param thickness: (meter)
+        :type thickness: float
+        :return: (ohm-metre)
+        :rtype: float
+        """
+        return sheet_resistance * thickness
+
+    @staticmethod
+    def from_resistance(resistance,  cross_sectional_area, length):
+        """ Find resistivity from resistance.
+
+        :param resistance: (ohm)
+        :type resistance: float
+        :param cross_sectional_area: (meter squared)
+        :type cross_sectional_area: float
+        :param length: (meter)
+        :type length: float
+        :return: (ohm-metre)
+        :rtype: float
+        """
+        return resistance * cross_sectional_area / length
