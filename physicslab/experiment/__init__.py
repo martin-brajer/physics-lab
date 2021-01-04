72848676849d5ed0212f22ab284e734d0f7c7503
Martin Brajer <martin.brajer@seznam.cz>
2021-01-04 18:33:32 +0100

    experiment process bacics

--- physicslab/experiment/__init__.py
+++ physicslab/experiment/__init__.py
@@ -2,4 +2,15 @@
 Modules for particular experiments.
 """
 
+
 import physicslab.experiment.van_der_pauw
+import physicslab.experiment.hall
+
+
+def process(measurements, by_module, **kwargs):
+    output = by_module.process(None)
+    for name, data in measurements:
+        series = by_module.process(data, **kwargs)
+        series.name = name
+        output = output.append(series)
+    return output
