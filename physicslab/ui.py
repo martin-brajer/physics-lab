f4248f0d7a773120be28df8f3d2ffdb3a515149d
Martin Brajer <martin.brajer@seznam.cz>
2021-05-27 17:28:38 +0200

    plot_grid rework

--- physicslab/ui.py
+++ physicslab/ui.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_grid(df, plot_value, xlabel=None, ylabel=None,
-              title_label=True, row_label=True, col_label=True,
-              legend=False, legend_size=10, show_axis=True,
-              subplots_adjust_kw=None, add_skip=None, **kwargs):
+def plot_grid(df, plot_value, fig_axs=None, skip=None,
+              title=None, xlabel=None, ylabel=None,
+              row_labels=True, column_labels=True,
+              subplots_adjust_kw=None, **kwargs):
     """ Construct a figure with the same layout as the input.
 
     For example, use it to display
@@ -18,90 +18,85 @@ def plot_grid(df, plot_value, xlabel=None, ylabel=None,
     images, where rows correspond to different magnifications and columns
     to samples.
 
-    If a :attr:`df` value is :obj:`None` or :obj:`numpy.nan`, skip the plot.
+    If a :attr:`df` value is :obj:`None` or :obj:`numpy.nan`, skip the
+    individual plot.
 
     :param df: Data to drive plotting. E.g. filename to load and plot
     :type df: pandas.DataFrame
-    :param plot_value: Function to convert :attr:`df` value into
-        ``ax.plot`` call. `<Signature (ax:Axis, value:object)>`
+    :param plot_value: Function to convert a :attr:`df` value into ``ax.plot``.
+
+        .. code:: python
+
+            def plot_value(ax: matplotlib.axes.Axes, value: object):
+                ax.plot(value.x)
+                ax.legend()  # Show a legend for each plot.
+
     :type plot_value: callable
+    :param fig_axs: Figure and axis array to draw to. Axis shape must match
+        that of :attr:`df`. If None, create a new figure, defaults to None
+    :type fig_axs: tuple(~matplotlib.figure.Figure,
+        ~numpy.ndarray(~matplotlib.axes.Axes)), optional
+    :param skip: Skip df values matching any of the listed items,
+        defaults to None
+    :type skip: list, optional
+    :param title: Common title. If ``'auto'``, use :attr:`df.name` if
+        available, defaults to None
+    :type title: str, optional
     :param xlabel: Common x axis label, defaults to None
     :type xlabel: str, optional
     :param ylabel: Common y axis label, defaults to None
     :type ylabel: str, optional
-    :param title_label: Set figure label to :attr:`df.name`, defaults to True
-    :type title_label: bool, optional
-    :param row_label: Annotate rows by :attr:`df.index`, defaults to True
-    :type row_label: bool, optional
-    :param col_label: Annotate columns by :attr:`df.columns`, defaults to True
-    :type col_label: bool, optional
-    :param legend: Show a legend for each plot, defaults to True
-    :type legend: bool, optional
-    :param legend_size: Legend box and font size, defaults to 10
-    :type legend_size: float, optional
-    :param show_axis: Visibility of axis, ticks and frame, defaults to True
-    :type show_axis: bool, optional
+    :param row_labels: Annotate rows by :attr:`df.index`, defaults to True
+    :type row_labels: bool, optional
+    :param column_labels: Annotate columns by :attr:`df.columns`,
+        defaults to True
+    :type column_labels: bool, optional
     :param subplots_adjust_kw: Dict with keywords passed to the
         :func:`~matplotlib.pyplot.subplots_adjust` call.
         E.g. ``hspace``, defaults to None
     :type subplots_adjust_kw: dict, optional
-    :param add_skip: If a df value matches one of listed items, skip it,
-        defaults to None
-    :type add_skip: list or None, optional
     :param kwargs: All additional keyword arguments are passed to the
-        :func:`~matplotlib.pyplot.figure` call. E.g. ``sharex``.
+        :func:`~matplotlib.pyplot.figure` call. E.g. ``sharex``
+    :raises ValueError: If :attr:`df` and :attr:`axs` have different shape
     """
-    title = df.name if (title_label and hasattr(df, 'name')) else None
-    if add_skip is None:
-        add_skip = []
-
+    figure_label = df.name if hasattr(df, 'name') else None
     nrows, ncols = df.shape
-    fig, axs = plt.subplots(num=title, nrows=nrows, ncols=ncols, **kwargs)
+    if fig_axs is None:
+        fig, axs = plt.subplots(
+            num=figure_label, nrows=nrows, ncols=ncols, **kwargs)
+    else:
+        fig, axs = fig_axs
+        if df.shape != axs.shape:
+            raise ValueError('axs and df shape must match')
 
-    for i, (ax_row, (index, row)) in enumerate(zip(axs, df.iterrows())):
-        for j, (ax, (col, value)) in enumerate(zip(ax_row, row.iteritems())):
-            if col_label and i == 0:  # First row.
-                ax.set_title(col)
-            if row_label and j == 0:  # First column.
+    for ax_row, (index, row) in zip(axs, df.iterrows()):
+        for ax, (column, value) in zip(ax_row, row.iteritems()):
+            if column_labels and ax.is_first_row():
+                ax.set_title(column)
+            if row_labels and ax.is_first_col():
                 ax.set_ylabel(row.name)
-            if (value is None or value in add_skip
-                    or isinstance(value, float) and np.isnan(value)):  # np.nan
-                _hide_axis(ax)
+            # Skipping this ax.
+            if (value is None
+                or (isinstance(value, float) and np.isnan(value))  # np.nan
+                    or (skip is not None and value in skip)):
+                # Like ``ax.axis('off')``, but keeps labels visible.
+                ax.tick_params(labelcolor='none',
+                               top=False, bottom=False,
+                               left=False, right=False)
+                ax.set_frame_on(False)
                 continue
-            if not show_axis:
-                _hide_axis(ax)
-            plot_value(ax, value)  # Main stuff happens here.
-            if legend:
-                ax.legend(prop={'size': legend_size})
+            plot_value(ax, value)  # The main stuff happens here.
 
-    # Common x and y labels.
+    # Common labels.
+    if title is not None:
+        if title == 'auto':
+            # If `df.name` doesn't exists, `figure_label` is None.
+            title = figure_label
+        fig.suptitle(title)
     if xlabel is not None:
         fig.text(0.5, 0.04, xlabel, ha='center')
+        # Change to fig.supxlabel(xlabel) from python 3.7 onward.
     if ylabel is not None:
         fig.text(0.04, 0.5, ylabel, va='center', rotation='vertical')
-
-    # plt.subplots_adjust()
-    default = {
-        'left': 0.10 if ylabel is None else 0.15,
-        'bottom': 0.10 if xlabel is None else 0.15,
-        'right': 0.95,
-        'top': 0.95,
-    }
-    if subplots_adjust_kw is None:
-        subplots_adjust_kw = {}
-    plt.subplots_adjust(**{**default, **subplots_adjust_kw})
-
-    plt.show()
-
-
-def _hide_axis(ax):
-    """ Like ``ax.axis('off')``, but keeps labels visible.
-
-    :param ax: Axis
-    :type ax: matplotlib.axes.Axes
-    """
-    ax.set_xticks([])
-    ax.set_xticklabels([])
-    ax.set_yticks([])
-    ax.set_yticklabels([])
-    ax.set_frame_on(False)
+    if subplots_adjust_kw is not None:
+        plt.subplots_adjust(**subplots_adjust_kw)
