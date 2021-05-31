70b6fcbd43c7afabfe9eb10bae265e832c624cab
Martin Brajer <martin.brajer@seznam.cz>
2021-05-31 20:43:30 +0200

    Print with units and ading units in process

--- physicslab/experiment/__init__.py
+++ physicslab/experiment/__init__.py
@@ -13,9 +13,15 @@ from physicslab.experiment import sem
 from physicslab.experiment import van_der_pauw
 
 
+#: :attr:`pandas.Dataframe.attrs` tag.
+UNITS = 'units'
+
+
 def process(data_list, by_module, **kwargs):
     """ Genereal process function calling appropriate :func:`process` function
     from selected :mod:`experiment` module.
+
+    Include units attribute.
 
     :param data_list: List of measurements, which are passed to the
         appropriate :func:`process` function
@@ -33,4 +39,25 @@ def process(data_list, by_module, **kwargs):
         series = by_module.process(data, **kwargs)
         series.name = data.name if hasattr(data, 'name') else i
         output.append(series)
-    return pd.DataFrame(output)
+
+    df = pd.DataFrame(output)
+    df.attrs[UNITS] = by_module.process(None)
+    return df
+
+
+def print_(df):
+    """ Print the dataframe, include units row if available.
+
+    Does not change the input DataFrame.
+
+    :param df:
+    :type df: pandas.DataFrame
+    """
+    # :attr:`pandas.Dataframe.attrs` is experimental. See:
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.attrs.html
+    if df.attrs and UNITS in df.attrs:
+        df_units = pd.DataFrame(df.attrs[UNITS]).transpose()  # Make it a row.
+
+        df = pd.concat([df_units, df], axis=0)  # Should do hard copy.
+
+    print(df)
