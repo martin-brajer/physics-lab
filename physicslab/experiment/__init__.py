5aebdb4e5cc83bd2244cfdec322170fbe8fedaf9
Martin Brajer <martin.brajer@seznam.cz>
2022-02-26 18:52:41 +0100

    root print refactor

--- physicslab/experiment/__init__.py
+++ physicslab/experiment/__init__.py
@@ -48,7 +48,7 @@ def process(data_list: list, by_module: ModuleType, **kwargs) -> pd.DataFrame:
     return df
 
 
-def print_(df: Union[pd.DataFrame, pd.Series]) -> None:
+def print(df: Union[pd.DataFrame, pd.Series]) -> None:
     """ Print the data including units row if available.
 
     | Does not change the input DataFrame.
@@ -58,16 +58,18 @@ def print_(df: Union[pd.DataFrame, pd.Series]) -> None:
     :param df: Data to be printed
     :type df: pandas.DataFrame or pandas.Series
     """
-    if isinstance(df, pd.Series):
-        df_new = pd.DataFrame(df).transpose()
-        if df.attrs and UNITS in df.attrs:
-            df_new.attrs[UNITS] = df.attrs[UNITS]
-        df = df_new
-
     # :attr:`pandas.Dataframe.attrs` is experimental. See:
     # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.attrs.html
     if df.attrs and UNITS in df.attrs:
-        df_units = pd.DataFrame(df.attrs[UNITS]).transpose()  # Make it a row.
+        units = df.attrs[UNITS]
+    else:
+        units = None
+    
+    if isinstance(df, pd.Series):  # Must be after units readout.
+        df = pd.DataFrame(df).transpose()
+
+    if units is not None:
+        df_units = pd.DataFrame(units).transpose()  # Make it a row.
         df = pd.concat([df_units, df], axis=0)  # Should do hard copy.
 
     print(df)
